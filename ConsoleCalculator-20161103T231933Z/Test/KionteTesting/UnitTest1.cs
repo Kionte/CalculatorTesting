0dda8ef8aaf35d469ce3994029395304416aca3f
kbrown17 <kbrown17@LAB-LIB-134-STU>
2016-11-04 10:29:58 -0700

    More decimal precision and also added to more test cases that falied both involving negatives

---
+++ ConsoleCalculator-20161103T231933Z/Test/KionteTesting/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public double Calc(string expression)
+        {
+            Calculator calc = new Calculator();
+            calc.Execute(expression);
+
+            return calc.Answer;
+
+        }
+        [TestMethod]
+        public void DecimalPrecision_M()
+        {
+            Assert.AreEqual(1, Calc("(1/3) * 3"));
+        }
+        [TestMethod]
+        public void DecimalPrecision_D()
+        {
+            Assert.AreEqual(3, Calc("1 / (1/3)"));
+        }
+        [TestMethod]
+        public void DecimalPrecision_NM()
+        {
+            Assert.AreEqual(-1, Calc("-(1/3) * 3"));
+        }
+        [TestMethod]
+        public void DecimalPrecision_ND()
+        {
+            Assert.AreEqual(-3, Calc("1 / -(1/3)"));
+        }
+
+    }
+}
