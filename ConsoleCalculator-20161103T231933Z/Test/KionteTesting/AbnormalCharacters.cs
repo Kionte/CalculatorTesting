0a7271ab2defb7e85f8521271ac3d7ead982b552
Kionté <kbrown17@my.whitworth.edu>
2016-11-04 13:49:08 -0700

    fixed summary

--- ConsoleCalculator-20161103T231933Z/Test/KionteTesting/AbnormalCharacters.cs
+++ ConsoleCalculator-20161103T231933Z/Test/KionteTesting/AbnormalCharacters.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ConsoleCalculator
 {
-    // This class will contain test cases that will perform expressions with different variations of deciaml values 
+    // This class will contain test cases that will test if the program can handle non mathmatic values
 
     [TestClass]
     class AbnormalCharacters
