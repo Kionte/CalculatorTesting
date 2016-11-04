9497be340a20363c8a030c3de37eb40a658dbc07
Kionté <kbrown17@my.whitworth.edu>
2016-11-04 13:47:41 -0700

    added file for abnormal character test cases

---
+++ ConsoleCalculator-20161103T231933Z/Test/KionteTesting/AbnormalCharacters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleCalculator
+{
+    // This class will contain test cases that will perform expressions with different variations of deciaml values 
+
+    [TestClass]
+    class AbnormalCharacters
+    {
+
+    }
+}
