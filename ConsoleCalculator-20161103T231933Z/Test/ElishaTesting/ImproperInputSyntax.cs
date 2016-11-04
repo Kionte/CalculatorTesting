3189f16e18549025a2ea21199142f730b666d512
eparslow18 <eparslow18@LAB-SCI-304-06>
2016-11-04 13:23:11 -0700

    Creating test cases to test impoper inputs by the user such as spaces and no operations and special characters

---
+++ ConsoleCalculator-20161103T231933Z/Test/ElishaTesting/ImproperInputSyntax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.ElishaTesting
+{
+    [TestClass]
+    public class ImproperInputSyntax
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
