ea09ebb14f2f6f8420185cbf7b5d3aad6129d2b1
eparslow18 <eparslow18@LAB-SCI-214-19>
2016-11-07 21:21:10 -0800

    More testing for exeptions with improper character input

--- ConsoleCalculator-20161103T231933Z/Test/ElishaTesting/ImproperInputSyntax.cs
+++ ConsoleCalculator-20161103T231933Z/Test/ElishaTesting/ImproperInputSyntax.cs
@@ -61,5 +61,262 @@ namespace ConsoleCalculator
             }
             Assert.AreEqual(ex, true);
         }
+
+        [TestMethod]
+        public void CharacterMinusNum()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b - 1");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+        [TestMethod]
+        public void CharacterTimesNum()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b * 1");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+        [TestMethod]
+        public void CharacterDivNum()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b / 1");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+        [TestMethod]
+        public void NumWithSpaces()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("5  5");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+        [TestMethod]//8
+        public void CharacterAssigningVariableaandAdd()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b = 1");
+                CalcSim("b + 5");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, false);
+        }
+
+        [TestMethod]
+        public void CharacterInitialized()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b = 1");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, false);
+        }
+
+        [TestMethod]//10
+        public void CharacterInitializedToChar()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b = c");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+
+        [TestMethod]//11
+        public void CharacterSpaceBracket()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b [");
+            }
+            catch
+            { 
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+
+        [TestMethod]//12
+        public void CharacteragdNum()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b4c");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+        [TestMethod]//13
+        public void CharacterNegative()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("-b");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+        [TestMethod]//14
+        public void CharacterPositive()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("+b");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+        [TestMethod]//15
+        public void CharacterEquals()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("=c");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+        [TestMethod]//16
+        public void IntEqual()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("=4");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+
+        [TestMethod]//17
+        public void IntSpaces()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("1    4");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+        [TestMethod]//18
+        public void CharacterAddingAssignedChars()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b = 2");
+                CalcSim("a = 3");
+                CalcSim("a + b");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, false);
+        }
+
+        [TestMethod]//19
+        public void CharacterPlusAscii()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("b + ∞");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
+        [TestMethod]//20
+        public void CharacterWithAcent()
+        {
+            bool ex = false;
+            try
+            {
+                CalcSim("à  +1");
+            }
+            catch
+            {
+                ex = true;
+            }
+            Assert.AreEqual(ex, true);
+        }
     }
 }
