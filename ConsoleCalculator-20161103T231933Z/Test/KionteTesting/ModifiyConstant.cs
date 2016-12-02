29c730fbb33b91260152641d653ffc24bc42a7bf
kbrown17 <kbrown17@LAB-SCI-304-07>
2016-12-02 13:23:41 -0800

    finihsed all my test cases

---
+++ ConsoleCalculator-20161103T231933Z/Test/KionteTesting/ModifiyConstant.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleCalculator
+{
+    // This class will contain test cases that will test if the program can handle non mathmatic values
+
+    [TestClass]
+    class ModifyPi
+    {
+        public double Calc(string expression)
+        {
+            Calculator calc = new Calculator();
+            calc.Execute(expression);
+
+            return calc.Answer;
+        }
+
+        [TestMethod]
+        public void ModConstant_Pi()
+        {
+            bool a = true;
+            try
+            {
+                Calc("pi = 1");
+
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+        public void ModConstant_E()
+        {
+            bool a = true;
+            try
+            {
+                Calc("e = 1");
+            }
+            catch(Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+        public void ModConstant_E_to_Pi()
+        {
+            bool a = true;
+            try
+            {
+                Calc("e = pi");
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+        public void ModConstant_Pi__to_E()
+        {
+            bool a = true;
+            try
+            {
+                Calc("pi = e");
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+        public void ModConstant_E_to_Err()
+        {
+            bool a = true;
+            try
+            {
+                Calc("e = '");
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+        public void ModConstant_Pi_to_Err()
+        {
+            bool a = true;
+            try
+            {
+                Calc("pi = '");
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+        public void ModConstant_Del_E()
+        {
+            bool a = true;
+            try
+            {
+                Calc("delete e");
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+        public void ModConstant_Del_PI()
+        {
+            bool a = true;
+            try
+            {
+                Calc("delete pi");
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+    }
+}
---
+++ ConsoleCalculator-20161103T231933Z/Test/KionteTesting/Parser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleCalculator
+{
+    // This class will contain test cases that test outside the limits of the program it should fail but be able to catrch and handle the excpetions
+
+    [TestClass]
+    public class Parser
+    {
+        public double Calc(string expression)
+        {
+            Calculator calc = new Calculator();
+            calc.Execute(expression);
+
+            return calc.Answer;
+        }
+        public void Delete_Non_Var()
+        {
+            bool a = true;
+            try
+            {
+                Calc("delete a");
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+        public void Change_Val_Of_Non_var()
+        {
+            bool a = true;
+            try
+            {
+                Calc("a + 1");
+            }
+            catch (Exception e)
+            {
+                a = false;
+            }
+            Assert.AreEqual(false, a);
+        }
+    }
+}
